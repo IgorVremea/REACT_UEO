80ea36832bac1f59b9828856a76993f3a34d4dff
Igor Vremea <igori.vremya@gmail.com>
2024-10-23 16:47:45 +0300

    Buttons

--- src/App.js
+++ src/App.js
@@ -3,38 +3,57 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [countries, setCountries]  = useState([]);
-  
-  useEffect(()=> {
-     
-      let unvisitedCountries = {};
-      let visitedCountries = {};
-      const response = fetch("https://restcountries.com/v3.1/independent?status=true&fields=name,area,population,region")
-          .then(response => response.json())
-          .then(data => setCountries(data));    
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  useEffect(() => {
+    const response = fetch("https://restcountries.com/v3.1/independent?status=true&fields=name,area,population,region")
+        .then(response => response.json())
+        .then(data => setCountries(data));
   }, []);
 
-  const groupedCountries = countries.reduce((acc, country) =>  {
-    if(!acc[country.region]){
+  const groupedCountries = countries.reduce((acc, country) => {
+    if (!acc[country.region]) {
       acc[country.region] = [];
     }
     acc[country.region].push(country);
     return acc;
-  }, {});
+  } , {});
+
+  const onVisitedClick = (country) => {
+    setVisitedCountries([...visitedCountries, country.name.common]);
+  }
+  const onRemove = (country) => {
+    setVisitedCountries(visitedCountries.filter(c => c !== country));
+  }
   return (
-    <div className="App">
+    <div className="App" style={{display: "flex"}}>
+      <div>
+      <h2>Unvisited Countries</h2>
       {Object.entries(groupedCountries).map(([region, countries]) => (
-        <div key = {region}>
-            <h2>{region}</h2>
-            <span>
-              {countries.map(country => (
-                <div key = {country.name.common}>
-                  <div>{country.name.common}</div>
-                </div>
-              ))}
-            </span>
+        <div key={region}>
+          <h2>{region}</h2>
+          <span>
+            {countries.filter(country => !visitedCountries.includes(country.name.common)).map(country => (
+              <div key={country.name.common}>
+                <div>{country.name.common}</div>
+                <button onClick={() => onVisitedClick(country)}>visited</button>
+              </div>
+            ))}
+          </span>
         </div>
       ))}
+      </div>
+      <div>
+        <h2>Visited Countries</h2>
+        <span>
+          {visitedCountries.map(country => (
+            <div>
+              <div key={country}>{country}</div>
+              <button onClick={() => onRemove(country)}>unvisited</button>
+            </div>
+          ))}
+        </span>
+      </div>
     </div>
   );
 }
